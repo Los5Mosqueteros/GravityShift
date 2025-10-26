1e68ba8384d201d94953011c9a33a5547aaa4ba3
CaseChips <ncasemasana04@gmail.com>
2025-10-26 10:58:33 +0100

    feat: basic projectile script

---
+++ GravityShift/Assets/Scripts/Projectile.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    public float speed = 20f;
+    public float lifetime = 3f;
+    private float spawnTime;
+
+    void Start()
+    {
+        spawnTime = Time.time;
+    }
+
+
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+
+        if (Time.time - spawnTime > lifetime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
