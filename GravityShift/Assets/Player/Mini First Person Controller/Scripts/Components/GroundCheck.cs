f106d39268d2dae78f297d6b9a07ecff5679cc3f
Cifu2004 <albertcbures@gmail.com>
2025-11-22 17:48:33 +0100

    FIX: Infinite jump on roof
    Now you can only jump once

--- GravityShift/Assets/Player/Mini First Person Controller/Scripts/Components/GroundCheck.cs
+++ GravityShift/Assets/Player/Mini First Person Controller/Scripts/Components/GroundCheck.cs
@@ -21,7 +21,7 @@ public class GroundCheck : MonoBehaviour
     void LateUpdate()
     {
         // Check if we are grounded now.
-        bool isGroundedNow = Physics.Raycast(RaycastOrigin, Vector3.down, distanceThreshold * 2);
+        bool isGroundedNow = Physics.Raycast(RaycastOrigin, -transform.up, distanceThreshold * 2);
 
         // Call event if we were in the air and we are now touching the ground.
         if (isGroundedNow && !isGrounded)
@@ -36,6 +36,6 @@ public class GroundCheck : MonoBehaviour
     void OnDrawGizmosSelected()
     {
         // Draw a line in the Editor to show whether we are touching the ground.
-        Debug.DrawLine(RaycastOrigin, RaycastOrigin + Vector3.down * RaycastDistance, isGrounded ? Color.white : Color.red);
+        Debug.DrawLine(RaycastOrigin, RaycastOrigin + -transform.up * RaycastDistance, isGrounded ? Color.white : Color.red);
     }
 }
