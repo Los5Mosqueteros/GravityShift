2910b0a6a6d3ae944dcfcdb744b8bfd28c585fe1
AlejandroIR <shibainuale@gmail.com>
2025-11-22 15:40:12 +0100

    Pistol scripts + animations

---
+++ GravityShift/Assets/Player/Mini First Person Controller/Scripts/Pistol.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class Pistol : MonoBehaviour
+{
+    [Header("Ammo")]
+    [SerializeField] private int maxAmmo = 12;
+    private int currentAmmo;
+
+    [Header("Shooting")]
+    [SerializeField] private float raycastDistance = 10f;
+
+    [Header("Effects")]
+    [SerializeField] private Transform muzzle;
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private float bulletSpeed = 60f;
+    [SerializeField] private Animator animator;
+    
+    private Camera mainCamera;
+
+    void Start()
+    {
+        currentAmmo = maxAmmo;
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && currentAmmo > 0)
+            animator.SetTrigger("Shoot");
+        
+        if (Input.GetKeyDown(KeyCode.R) && currentAmmo < maxAmmo)
+            animator.SetTrigger("Reload");
+    }
+
+    public void Shoot()
+    {
+        if (currentAmmo <= 0) return;
+
+        currentAmmo--;
+
+        Ray ray = mainCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        Vector3 targetPoint = ray.origin + ray.direction * raycastDistance;
+        Vector3 shootDirection = (targetPoint - muzzle.position).normalized;
+        
+        GameObject bullet = Instantiate(bulletPrefab, muzzle.position, Quaternion.FromToRotation(Vector3.left, shootDirection));
+        Projectile projectile = bullet.GetComponent<Projectile>();
+        
+        projectile.speed = bulletSpeed;
+        projectile.SetConvergenceData(targetPoint, ray.direction);
+    }
+
+    public void Reload()
+    {
+        currentAmmo = maxAmmo;
+    }
+
+    public int GetCurrentAmmo() => currentAmmo;
+    public int GetMaxAmmo() => maxAmmo;
+}
---
+++ GravityShift/Assets/Scripts/PistolProjectile.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PistolProjectile : Projectile
+{
+    [SerializeField] private float damage = 15f;
+
+    void OnTriggerEnter(Collider collision)
+    {
+        Destroy(gameObject);
+    }
+
+    public float GetDamage() => damage;
+}
